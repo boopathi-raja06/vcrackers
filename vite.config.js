9c86369ef6ea2c148b7c7aa3976b090c0f136799
boopathi-raja06 <boopathraja060700@gmail.com>
2025-09-11 10:59:33 +0530

    Update for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/vcrackers/', // Set to your repo name for GitHub Pages
+  plugins: [react()]
 })
